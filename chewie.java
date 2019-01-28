83097b88afd212384143080b1de8c6132edc8d33
a-d-a-m-s <44054313+a-d-a-m-s@users.noreply.github.com>
2019-01-28 12:37:13 +0100

    Update chewie.java

--- chewie.java
+++ chewie.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-Actions actionList;
-actionList = new Actions(driver);
+
 WebElement webElement;
 webElement = driver.findElement(By.cssSelector("[id="img_slideboard"]"));
