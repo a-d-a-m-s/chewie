875f9b81bd0337eebe882ff92c7b5fe0db222ca7
a-d-a-m-s <44054313+a-d-a-m-s@users.noreply.github.com>
2019-01-28 15:47:03 +0100

    Update chewie.java

--- chewie.java
+++ chewie.java
@@ -2,6 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+public class chewie {
+  WebElement webElement;
+  webElement = driver.findElement(By.cssSelector("[id="img_slideboard"]")).requireAndClick();
+  public void setDefineEndShareField_PrevMonth(int x) {
+  }
 
-WebElement webElement;
-webElement = driver.findElement(By.cssSelector("[id="img_slideboard"]"));
+}
